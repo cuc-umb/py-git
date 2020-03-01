8c1be0eaf0ac889e0759db388f125b9eabfd5502
cuc-umb <null.f@yahoo.com>
2020-03-01 16:39:11 +0700

    Добавлен выбор заданий

--- per.py
+++ per.py
@@ -1,12 +1,15 @@
 import random
- 
-actors = ['Смычкова Татьяна','Скрипкина Светлана','Саксофонова Ирина',
+
+NofEx = int(input('Введите номер задания: '))
+
+if NofEx == 4:
+    actors = ['Смычкова Татьяна','Скрипкина Светлана','Саксофонова Ирина',
           'Трубников Олег','Баянов Святослав','Балалайкин Валентин',
           'Басов Рустем','Песняр Ярослав','Музычко Игорь','Альтова Елена']
-str1 = ''; str2 = ''; str3 = ''
-print('Первый этап')
-random.shuffle(actors); str1 = ''.join(actors)+ "\n"; print(str1)
-print('Второй этап')
-random.shuffle(actors); str2 = ''.join(actors)+ "\n"; print(str2)
-print('Третий этап')
-random.shuffle(actors); str3 = ''.join(actors)+ "\n"; print(str3)
+    str1 = ''; str2 = ''; str3 = ''
+    print('Первый этап')
+    random.shuffle(actors); str1 = ''.join(actors)+ "\n"; print(str1)
+    print('Второй этап')
+    random.shuffle(actors); str2 = ''.join(actors)+ "\n"; print(str2)
+    print('Третий этап')
+    random.shuffle(actors); str3 = ''.join(actors)+ "\n"; print(str3)
